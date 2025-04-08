a929ccd4bdff345429bdca0ccb2a50cb1d32f1dc
JonusBurger <j.bergner@live.de>
2025-04-08 20:53:11 +0200

    setup gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,14 +1,45 @@
+const Ship = require("./ship");
+
 function gameboard() {
+    const FIELDHEIGTH = 10;
+    const FIELDLENGTH = 10;
     const ships = {
         2: undefined,
         3: undefined,
         4: undefined, 
         5: undefined
     }
+    let gameField = Array(FIELDHEIGTH).fill(Array(FIELDLENGTH).fill(0));
+
+    function placeShip(length, position, horizontal = false) {
+        if (!(length in ships)) {
+            return false
+        }
+
+        if (!shipPositionValidator(length, position, horizontal)) {
+            return false
+        }
 
 
-    function placeShip() {
-        return false
+        const ship = new Ship(length);
+        ships[length] = ship;
+        return true
+    }
+
+    function shipPositionValidator(length, position, horizontal) {
+        if (horizontal) {
+            if (position[0] + length >= FIELDLENGTH) {
+                return false
+            }
+        } else {
+            if (position[1] + length >= FIELDHEIGTH) {
+                return false
+            }
+        } if (position[0] >= FIELDLENGTH || position[1] >= FIELDHEIGTH) {
+            return false
+        }
+        
+        return true
     }
 
     return {
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -10,4 +10,12 @@ test("A ship may only be placed at a valid position", () => {
     const gameboardInstance = Gameboard();
 
     expect(gameboardInstance.placeShip(3, [1,1], horizontal = true)).toBeTruthy();
+    expect(gameboardInstance.placeShip(3, [1,9], horizontal = false)).toBeFalsy();
+}) 
+
+test("A ship cannot be placed upon another ship", () => {
+    const gameboardInstance = Gameboard();
+    gameboardInstance.placeShip(3, [1,1], horizontal = true)
+    
+    expect(gameboardInstance.placeShip(3, [0,1], horizontal = true)).toBeFalsy();
 }) 
