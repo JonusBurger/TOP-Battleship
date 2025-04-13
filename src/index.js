15a6dcf199c2222aadb428d67bc9e64ba3aa94de
Jberg <j.bergner@live.de>
2025-04-13 21:33:55 +0200

    update Info

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ const eventHandler = require("./eventhandler");
 const eventHandlerInstance = eventHandler();
 
 // NS:
+// Fix Bug building GameField
 // Add Logic to access correct html part depending on active player
 // Implement Recieve Attack on click of Field
